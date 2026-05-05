ea653ab3f658381f412dda144f232c34cb49d28d
D3XX15 <dennisvankooten3@gmail.com>
2026-05-05 23:42:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2391,7 +2391,7 @@ function SetupScreen({ onStart, gameMode, setGameMode, lsNames, setLsNames, onSt
                 >
                   <span className="ls-mode-icon">💬</span>
                   <span className="ls-mode-title">Klassiek</span>
-                  <span className="ls-mode-desc">Leg of beeld het woord zo duidelijk mogelijk uit.</span>
+                  <span className="ls-mode-desc">Leg of beeld het woord uit terwijl de andere raden.</span>
                 </button>
                 <button
                   className={`ls-mode-btn ${wrGameMode === "taboe" ? "ls-mode-btn-active" : "ls-mode-btn-inactive"}`}
